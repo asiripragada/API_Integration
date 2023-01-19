2cdf54ee6bc9550933107858fc1116c4b4a1e7f9
asiripragada <92725571+asiripragada@users.noreply.github.com>
2023-01-19 20:14:11 +0530

    Update views.py

--- calculator/views.py
+++ calculator/views.py
@@ -25,7 +25,7 @@ def addition(request):
         # Create the pandas DataFrame
 #         df = pd.DataFrame(data, columns=['key', 'value'])
 
-        return HttpResponse(res),HttpResponse(res1)
+        return HttpResponse(int(res))
     else:
         res = "Only digits are allowed"
         return render(request, "result.html", {"result": res})
